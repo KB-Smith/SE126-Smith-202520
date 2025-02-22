513d29f9409dc19171cf056f55e9d933d34741b8
KB-Smith <kbsmith@email.neit.edu>
2025-02-21 20:26:02 -0500

    Update W7 Lab 5.py

--- Week 7/W7 Lab 5.py
+++ Week 7/W7 Lab 5.py
@@ -1,9 +1,38 @@
+#Kyle Smith
+#SE126.04
+#Lab 4
+#2-21-2025
+
+#PROGRAM PROMPT:tore the file data into 1D parallel lists, then use the appropriate searching algorithms for the menu system options.
+#Your program should give your user the following menu:
+#Personal Library Menu
+#1. Show All Titles – list all book data to the user alphabetically by title
+#2. Search by Title – allow for an entire title or a title key word
+#3. Search by Author – show all titles of the searched-for author
+#4. Search by Genre - show all titles of the searched-for genre
+#5. Search by Library Number – only allow for one specific library number item
+#6. Show All Available – show all titles with status “available”
+#7. Show All On Loan - show all titles with status “on loan”
+#8. EXIT
+#When your user runs any of the options 1 – 7, show all data associated with the search [Library Number, Title, Author,
+#Genre, Page count, Status]. Do not allow the program to end unless the user chooses option 8 to exit. All searches
+#should not be case sensitive.
+#---------------------------------------------------------
+#Variable Dictionary:
+
+#total_records: Total number of records
+#user_choice: Stores numbers for menu options
+
+
+
+
+
 #Imports------------------
 import csv
 import time # Allows for sleep
 #Functions-------------------
 def menu():#<- main menu function
-    print("Personal Library Menu")
+    print("~Personal Library Menu~")
     print("1. Show All Titles")
     print("2. Search by Title")
     print("3. Search by Author") 
@@ -50,14 +79,16 @@ with open ("text_files/book_list.csv") as csvfile:
         page_count.append(rec[4])
         status.append(rec[5])
 
-print("----------------------------------------------------------------------------------------------------------------------------------------------------")
-print(f"{'LIBRARY NUM':4}\t{'TITLE':30}\t\t{'AUTHOR':35}\t{'GENRE':20}\t{'PAGE#':3}\t\t{'STATUS':10}")   
-print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+#print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+#print(f"{'LIBRARY NUM':4}\t{'TITLE':30}\t\t{'AUTHOR':35}\t{'GENRE':20}\t{'PAGE#':3}\t\t{'STATUS':10}")   
+#print("----------------------------------------------------------------------------------------------------------------------------------------------------")
 
-for index in range(0, len(library_num)):
-     print(f"{library_num[index]:4}\t\t{title[index]:34}    {author[index]:35}\t{genre[index]:20}\t{page_count[index]:3}   \t\t{status[index]:10}")
-print("------------------------------------------------------------------------------------------------------------------------------------------\n")
+#for index in range(0, len(library_num)):
+    # print(f"{library_num[index]:4}\t\t{title[index]:34}    {author[index]:35}\t{genre[index]:20}\t{page_count[index]:3}   \t\t{status[index]:10}")
+#print("------------------------------------------------------------------------------------------------------------------------------------------\n")
 
+
+#--MAIN EXECUTING CODE----------------------
 
 while user_choice != "8":
     user_choice = menu()
@@ -74,14 +105,16 @@ while user_choice != "8":
                     title[k + 1] = temp
 
                    
-    
+            # Prints Collumn Headers
         print("----------------------------------------------------------------------------------------------------------------------------------------------------")
         print(f"{'LIBRARY NUM':4}\t{'TITLE':30}\t\t{'AUTHOR':35}\t{'GENRE':20}\t{'PAGE#':3}\t\t{'STATUS':10}")   
         print("----------------------------------------------------------------------------------------------------------------------------------------------------")
 
+            #printing the data
         for index in range(0, len(library_num)):
             print(f"{library_num[index]:4}\t\t{title[index]:34}    {author[index]:35}\t{genre[index]:20}\t{page_count[index]:3}   \t\t{status[index]:10}")
             print("------------------------------------------------------------------------------------------------------------------------------------------\n")
+
 
     elif user_choice == "2":
 
@@ -109,7 +142,7 @@ while user_choice != "8":
             for index in found:
                 
                 print(f"{library_num[index]:4}\t\t{title[index]:34}    {author[index]:35}\t{genre[index]:20}\t{page_count[index]:3}   \t\t{status[index]:10}")
-                print("---------------------------------------------------------------------------------------------------------------------------------------------------n")
+                print("---------------------------------------------------------------------------------------------------------------------------------------------------")
 
 
             
