cf010d2c907bb87dad3467cf6d266f055faf5587
KB-Smith <kbsmith@email.neit.edu>
2025-03-03 17:08:27 -0500

    W9D1 Update

--- Week 8/K.Smith_W8D1 Lab6.py
+++ Week 8/K.Smith_W8D1 Lab6.py
@@ -30,8 +30,10 @@ def seating_map():
     b = []
     c = []
     d = []
+    e = []
+    f = []
 
-    seat_setup = [a,b,c,d]
+    seat_setup = [a,b,c,d,e,f]
 
     with open ("text_files/plane.csv") as csvfile:
     # reading the CSV file
@@ -42,12 +44,14 @@ def seating_map():
             b.append(rec[1])
             c.append(rec[2])
             d.append(rec[3])
+            e.append(rec[4])
+            f.append(rec[5])
     
     print("Row")
     for index in range(0,len(a)):
-        print(f" {index+1}  {a[index]} {b[index]} {c[index]} {d[index]}")
+        print(f" {index+1:2}  {a[index]} {b[index]} {c[index]}  {d[index]} {e[index]} {f[index]}")
  
-    return a,b,c,d
+    return a,b,c,d,e,f
 
 def again():
 
@@ -66,12 +70,12 @@ while answer == "y":
     
         row = int(input("Enter the row you want to sit in: "))
 
-        if row not in [1,2,3,4,5,6,7]:
-            row = int(input("INVALID ENTRY!!! Please Choose A Row 1-7: "))
+        if row not in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]:
+            row = int(input("INVALID ENTRY!!! Please Choose A Row 1-24: "))
         
     except ValueError:
 
-        row = int(input("Try Again ERROR!! Choose A ROWWW!! 1-7: "))
+        row = int(input("Try Again ERROR!! Choose A ROWWW!! 1-24: "))
 
 
     index = row -1
@@ -79,15 +83,15 @@ while answer == "y":
     # to handle errors for seats
     try:
 
-        seat = input(f"Enter the seat (A, B, C, D): ").upper()
+        seat = input(f"Enter the seat (A, B, C,D,E,F): ").upper()
 
-        while seat not in ["A","B","C","D"]:
-            seat = input ("INVALID ENTRY!!! Please Choose A Seat (A,B,C or D): ").upper()
+        while seat not in ["A","B","C","D","E","F"]:
+            seat = input ("INVALID ENTRY!!! Please Choose A Seat (A,B,C,D,E,F): ").upper()
 
     
 
     except ValueError:
-        seat = input (("PLEASE READ THE INSTRUCTIONS AND CHOOSE A SEAT (A, B, C or D)!!: ")).upper
+        seat = input (("PLEASE READ THE INSTRUCTIONS AND CHOOSE A SEAT (A, B, C, D, E, F)!!: ")).upper
 
 
 
@@ -97,8 +101,12 @@ while answer == "y":
         seat_column = 1
     elif seat == "C":
         seat_column = 2
-    else:
+    elif seat == "D":
         seat_column = 3
+    elif seat == "E":
+        seat_column = 4
+    else:
+        seat_column = 5
 
 
     if seat_setup[seat_column][index] != "X":
@@ -110,7 +118,7 @@ while answer == "y":
 
     print("\nSeats Available:") #updates map seating
     for i in range(len(seat_setup[0])):
-        print(f"Row {i + 1}: {seat_setup[0][i]} {seat_setup[1][i]} {seat_setup[2][i]} {seat_setup[3][i]}")
+        print(f"Row {i + 1:2}: {seat_setup[0][i]} {seat_setup[1][i]} {seat_setup[2][i]} {seat_setup[3][i]} {seat_setup[4][i]} {seat_setup[5][i]}")
 
     answer = again()
 
--- Week 9/W9D1 Lab7.py
+++ Week 9/W9D1 Lab7.py
@@ -5,6 +5,7 @@ import csv
 #--MAIN EXECUTING CODE------------------------------------------------
 
 library = {}
+num_words = 0
 
 with open ("text_files/words.csv") as csvfile:
     file = csv.reader(csvfile)
@@ -32,4 +33,27 @@ if found != 0:
     print(f"\nWord: {found:6}\tDefinition: {library[found]:50}")
 else:
     print(f"\nYour search for {search} came up empty. Sorry :/ ")
-    
+
+num_words = int(input("Enter the number of words you wish to add: "))
+
+for key in range(num_words): 
+    key = input("Enter the word: ")
+    value = input("Enter the definition: ")
+    library[key] = value
+
+if key in library:
+        print("This word already exists in the dictionary already")
+        print(f"{key} has now been added to the dictionary!!")
+
+else:
+        print(f"{key} has been added to the dictionary!!")
+        library.update({rec[0] : rec[1]})
+          
+
+
+print(f"\n{'WORD'}\t\t{'DEFINITION':30}")
+print("-" * 100)
+for key in library:
+    #for every key (and value) pair found within the 'library' dictionary
+    print(f"{key.ljust(15)}\t{library[key]}")
+print("-" * 100)
